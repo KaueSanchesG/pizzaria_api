4f6ed8e76127a7eb9af61c6337ea2d09fb101686
KaueSanchesG <kauesanches123@gmail.com>
2023-08-17 19:46:10 -0300

    ClienteController Semi-Pronto

--- src/main/java/pizzaria/pizzaria/Controller/ClienteController.java
+++ src/main/java/pizzaria/pizzaria/Controller/ClienteController.java
@@ -1,11 +1,10 @@
 package pizzaria.pizzaria.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pizzaria.pizzaria.DTO.ClienteDTO;
 import pizzaria.pizzaria.Entity.ClienteEntity;
 import pizzaria.pizzaria.Repository.ClienteRepository;
 import pizzaria.pizzaria.Service.ClienteService;
@@ -18,7 +17,7 @@ public class ClienteController {
     @Autowired
     private ClienteRepository repository;
 
-    @GetMapping
+    @GetMapping("/lista")
     public ResponseEntity<?> list() {
         return ResponseEntity.ok(this.repository.findAll());
     }
@@ -29,5 +28,35 @@ public class ClienteController {
         return cliente == null ? ResponseEntity.badRequest().body("Sem valores encontrados") : ResponseEntity.ok(cliente);
     }
 
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody ClienteDTO clienteDTO){
+        try {
+            service.create(clienteDTO);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Erro " + e.getMessage());
+        }
+        return ResponseEntity.ok().body("Reistro cadastrado com sucesso!");
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateByPath(@PathVariable("id") final Long id, @RequestBody final ClienteDTO clienteDTO){
+        try {
+            service.update(id, clienteDTO);
+        }catch (DataIntegrityViolationException e){
+            return ResponseEntity.badRequest().body("Erro "+ e.getCause().getCause().getMessage());
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Erro "+ e.getMessage());
+        }
+        return ResponseEntity.ok("Registro editado com sucesso!");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id")final Long id){
+        try {
+            service.delete(id);
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body("Não foi possivel deletar");
+        }
+        return ResponseEntity.ok("Registro deletado!");
+    }
 }
