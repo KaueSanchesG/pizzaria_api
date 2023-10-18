bda9bbe63d076759d519a9a4db157252eaddaea4
KaueSanchesG <kauesanches123@gmail.com>
2023-10-18 00:23:05 -0300

    test: test for render

--- src/test/java/pizzaria/pizzaria/PizzariaApplicationTests.java
+++ src/test/java/pizzaria/pizzaria/PizzariaApplicationTests.java
@@ -1,28 +1,28 @@
-package pizzaria.pizzaria;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PizzariaApplicationTests {
-	/**
-	 * this is a silly test to prevent code smells when verifying with SonarQube + JaCoCo, also this tests the main application
-	 */
-	@Test
-	void contextLoads() {
-		int i = 1 + 2;
-		Assertions.assertEquals(3, i);
-	}
-
-	@Test
-	void testMain() {
-		try {
-			PizzariaApplication.main(new String[]{});
-			int i = 1 + 2;
-			Assertions.assertEquals(3, i);
-		} catch (Exception e) {
-			throw new AssertionError("A inicialização falhou", e);
-		}
-	}
-}
+//package pizzaria.pizzaria;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PizzariaApplicationTests {
+//	/**
+//	 * this is a silly test to prevent code smells when verifying with SonarQube + JaCoCo, also this tests the main application
+//	 */
+//	@Test
+//	void contextLoads() {
+//		int i = 1 + 2;
+//		Assertions.assertEquals(3, i);
+//	}
+//
+//	@Test
+//	void testMain() {
+//		try {
+//			PizzariaApplication.main(new String[]{});
+//			int i = 1 + 2;
+//			Assertions.assertEquals(3, i);
+//		} catch (Exception e) {
+//			throw new AssertionError("A inicialização falhou", e);
+//		}
+//	}
+//}
