66127203f3cefe5574cfd46ec412af1873dfbbd7
KaueSanchesG <kauesanches123@gmail.com>
2023-10-24 10:14:35 -0300

    feat: implementing the possibility of numbers on the credentials

--- src/main/java/pizzaria/pizzaria/repository/FuncionarioRepository.java
+++ src/main/java/pizzaria/pizzaria/repository/FuncionarioRepository.java
@@ -1,12 +1,15 @@
 package pizzaria.pizzaria.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pizzaria.pizzaria.entity.FuncionarioEntity;
 
 import java.util.List;
 
 public interface FuncionarioRepository extends JpaRepository<FuncionarioEntity, Long> {
-    FuncionarioEntity findByLogin(String login);
+    @Query("SELECT f FROM FuncionarioEntity f WHERE f.login =:login")
+    FuncionarioEntity findByLogin(@Param("login") String login);
 
     List<FuncionarioEntity> findByNome(String nome);
 }
